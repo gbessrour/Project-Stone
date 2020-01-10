4241ce29e4c13789d7b582b188c3132b2afdd195
Travis Le <travle58@hotmail.com>
2020-01-09 23:01:19 -0500

    modified whitespace for visibility and converted hello component into its own separate function

--- mechaSenku.py
+++ mechaSenku.py
@@ -19,11 +19,11 @@ jikan = Jikan()
 url = "http://arepublixchickentendersubsonsale.com"
 # Function to convert number into coin side
 def numbers_to_side(argument):
-	switcher = {
-		1: "heads",
-		2: "tails",
-	}
-	return switcher.get(argument, "nothing")
+    switcher = {
+        1: "heads",
+        2: "tails",
+    }
+    return switcher.get(argument, "nothing")
 
 # 8-ball responses
 possible_responses = [
@@ -40,25 +40,29 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
+
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
+
     # Simple welcome message
     elif message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}. How can Mecha Senku assist you today?'.format(message)
-        await message.channel.send(msg)
+        hello_message(message)
+
     # Dice roll
     elif message.content.startswith('!dice'):
         dice = random.randint(1, 6)
         filename = "dice " + str(dice) + ".png"
         dicename = os.path.join('dice', filename)
         await message.channel.send('You rolled ' + str(dice), file=discord.File(dicename))
+
     # Coin flip
     elif message.content.startswith('!coin'):
         coin = random.randint(1,2)
         filename = numbers_to_side(coin) + ".jpg"
         coinname = os.path.join('coin', filename)
         await message.channel.send('You got ' + numbers_to_side(coin), file=discord.File(coinname))
+
     # Dad Jokes
     elif (message.content.startswith('I\'m ')) or (message.content.startswith('I am ') or (message.content.startswith('Im '))):
         if message.content.startswith('I\'m '):
@@ -69,12 +73,14 @@ async def on_message(message):
             dadjoke = message.content.replace('Im ', '')
         msg = 'Hi' + dadjoke + '. I\'m dad!'
         await message.channel.send(msg)
+
     # Eight ball
     elif message.content.startswith('!8ball'):
         if ('win' in message.content) and ('lottery' in message.content):
             await message.channel.send('Statistically, the odds of winning are about 1 in 175 million so I would say no.')
         else:
             await message.channel.send(random.choice(possible_responses))
+
     # Cryptocurrency price
     elif message.content.startswith('!price'):
         message_list = message.content.split()
@@ -82,9 +88,11 @@ async def on_message(message):
         result = str(cg.get_price(ids=crypto, vs_currencies='usd'))
         price =  re.findall(r"\d+\.\d{1,2}", result)
         await message.channel.send(crypto +" price is: $" + price[0])
+
     # Confusion message
-    elif message.content == 'what' or message.content == 'wot' or message.content == 'wat':
-        await message.channel.send('what')
+    elif message.content == 'what' or message.content == 'wot' or message.content == 'wat' or message.content =='nani':
+        await message.channel.send(message.content)
+
     #PubSubs on sale or not
     elif message.content.startswith('!pubsub'):
         req = urllib.request.Request(url)
@@ -96,6 +104,8 @@ async def on_message(message):
             answer = "Pub subs ARE on sale my dudes!!!"
             await message.channel.send(file=discord.File(os.path.join('Reacts', 'excited_deku.gif')))
         await message.channel.send(answer)
+    
+    #Anime
     elif message.content.startswith('!anime'):
         anime_list = message.content.split()
         param = anime_list[anime_list.index('!anime') + 1]
@@ -122,6 +132,11 @@ async def on_message(message):
             third_param = anime_list[anime_list.index('!anime') + 3]
             result = jikan.season(year= int(third_param), season= second_param)
         await message.channel.send(embed=embed)
+
+def hello_message(msg):
+    str = 'Hello {0.author.mention}. How can Mecha Senku assist you today?'.format(msg)
+        await message.channel.send(str)
+
 @client.event
 async def on_ready():
     print('Logged in as')
