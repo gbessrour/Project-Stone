eb150ce8bd173c9b53cc1b6e93bd9e57787a939c
Ghassen Bessrour <33112672+gbessrour@users.noreply.github.com>
2020-01-17 14:28:14 -0500

    Delete .upload.sh

--- .upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Pulls and adds files to git before commit
-function upload(){
-	read -p "Commit description: " desc
-	git add . && \
-	git add -u && \
-	git commit -m "$desc" && \
-	git push origin master
-}
